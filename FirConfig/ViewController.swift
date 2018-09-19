0eb975f8b632cf154429564f1827145ddc3c8c32
Preuttipan <terrorize.nut@gmail.com>
2018-09-19 17:05:12 +0700

    Initial Commit

---
+++ FirConfig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirConfig
+//
+//  Created by Preuttipan Janpen on 19/9/2561 BE.
+//  Copyright © 2561 Lphant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
